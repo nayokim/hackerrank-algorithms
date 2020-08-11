545a62e5400ce2949ca0ce9451b3e3027e411e06
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-08-11 12:30:12 -0500

    completed

--- src/BreakingTheRecord/BreakingTheRecordI.java
+++ src/BreakingTheRecord/BreakingTheRecordI.java
@@ -6,10 +6,9 @@ public class BreakingTheRecordI {
     public static void main(String[] args) {
         //going through the loop
         //3 4 21 36 10 28 35 5 24 42
-        //sort from min to max -> add to new array
 
         int [] scores = {3,4,21,36,10,28,35,5,24,42};
-        int [] sortedScores =new int [scores.length];
+        int [] sortedScores =new int [2];
 
         // after each iteration, compare the first to the second.
         int counter = 0;
@@ -30,9 +29,13 @@ public class BreakingTheRecordI {
             }
 
         }
-        System.out.println(counter); //  number of times she broke her record
+        System.out.println(counter); //  number of times she broke her best record
 
-        System.out.println(minCounter);
+        System.out.println(minCounter); // number of times she broke her worst record
+
+        sortedScores[0] = counter;
+        sortedScores[1] = minCounter;
+
 
 
 
